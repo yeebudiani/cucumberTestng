653233be7274bc9b28fedeab500ede96b56aa107
yeebudiani <yeebudianil1@gmail.com>
2022-11-05 23:39:17 +0530

    new code

--- src/test/java/awesomecucumber/pages/BasePage.java
+++ src/test/java/awesomecucumber/pages/BasePage.java
@@ -19,7 +19,7 @@ public class BasePage
     public BasePage(WebDriver driver)
     {
         this.driver=driver;
-        wait=new WebDriverWait(driver, Duration.ofSeconds(3s0));
+        wait=new WebDriverWait(driver, Duration.ofSeconds(30));
         PageFactory.initElements(driver,this);
     }
 
