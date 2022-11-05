7e3733ae5b7e79292478c8509429cabfb2180a82
yeebudiani <yeebudianil1@gmail.com>
2022-11-05 21:32:41 +0530

    changed the time duration

--- src/test/java/awesomecucumber/pages/BasePage.java
+++ src/test/java/awesomecucumber/pages/BasePage.java
@@ -19,7 +19,7 @@ public class BasePage
     public BasePage(WebDriver driver)
     {
         this.driver=driver;
-        wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait=new WebDriverWait(driver, Duration.ofSeconds(40));
         PageFactory.initElements(driver,this);
     }
 
